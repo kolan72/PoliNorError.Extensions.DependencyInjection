c48a0b49735eb113e4b291a619290cce50130125
kolan72 <kolan72@gmail.com>
2026-05-03 23:04:38 +0300

    Add xml doc to `ISetConfigurator`.

--- src/Internal/ISetConfigurator.cs
+++ src/Internal/ISetConfigurator.cs
@@ -1,7 +1,18 @@
 ﻿namespace PoliNorError.Extensions.DependencyInjection
 {
+	/// <summary>
+	/// Internal seam that allows <see cref="ProxyPolicy{TBuilder}"/> to inject a
+	/// <see cref="PolicyConfigurator{TPolicy}"/> into a
+	/// <see cref="PolicyBuilder{TPolicy,TConfigurator,TBuilder}"/> before
+	/// <see cref="IPolicyBuilder{TBuilder}.Build"/> is called.
+	/// </summary>
 	internal interface ISetConfigurator
 	{
+		/// <summary>
+		/// Resolves the required <see cref="PolicyConfigurator{TPolicy}"/> from the DI container
+		/// and stores it for use during <see cref="IPolicyBuilder{TBuilder}.Build"/>.
+		/// </summary>
+		/// <param name="serviceProvider">The DI service provider.</param>
 		void SetConfigurator(IServiceProvider serviceProvider);
 	}
 }
